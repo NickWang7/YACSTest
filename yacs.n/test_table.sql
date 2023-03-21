9701eca1ad46ad9e6e8e1bc8f2602f7563e2c63b
chickenwang6 <122407085+chickenwang6@users.noreply.github.com>
2023-03-21 16:58:28 -0400

    table finished (?), check in with harry

--- yacs.n/test_table.sql
+++ yacs.n/test_table.sql
@@ -1,47 +1,116 @@
-CREATE TABLE CLASSES(
-    id SERIAL PRIMARY KEY,
-    category TEXT,
-    pathway TEXT);
-    -- required TEXT, 
-    -- choose TEXT, 
-    -- remaining TEXT, 
-    -- minors TEXT);
+-- MAKE SURE TO OPEN NEW QUERY TO RUN THINGS
 
--- CREATE TABLE CDI(
+
+
+-- CREATE TABLE CLASSES(
 --     id SERIAL PRIMARY KEY,
---     choose TEXT,
+--     category TEXT);
+
+-- CREATE TABLE PATHWAYS(
+--     id SERIAL PRIMARY KEY,
+--     classname TEXT,
+--     firstchoose text[],
+--     nextchoose text[],
+--     minors text[],
 --     user_id INTEGER,
 --     CONSTRAINT fk_user
 --         FOREIGN KEY(user_id)
 --             REFERENCES CLASSES(id)
 --     );
 
--- INSERT INTO CLASSES(category, pathway, required, choose, remaining, minors) VALUES
--- ('"Arts / Designs"', '"Creative Design and Innovation"', ' ', ' ', ' ', ' '); 
+-- UPDATE PATHWAYS
+-- SET required = '{"STSO 2520 - Sociology"}'
+-- WHERE id = 45;
+
+-- UPDATE PATHWAYS
+-- SET firstchoose = '{"IHSS 1150 - The Genome and You",
+--                     "PSYC 1200 - Introduction to Psychological Science",
+--                     "STSO 1110 - Science, Technology, and Society"}'
+-- WHERE id = 45;
+
+-- UPDATE PATHWAYS
+-- SET nextchoose = '{"COGS 4610 - Stress and the Brain",
+--                     "COGS 4700 - Hormones, Brain, and Behavior",
+--                     "PSYC 4500 - Drugs, Society, and Behavior",
+--                     "PSYC 4610 - Stress and the Brain",
+--                     "PSYC 4700 - Hormones, Brain, and Behavior",
+--                     "STSO 4420 - History of Medicine",
+--                     "STSO 4440 - History of Mental Health",
+--                     "STSO 4250 - Bioethics",
+--                     "STSO 4430 - Drugs in History",
+--                     "STSO 4260 - Food, Farms, and Famine",
+--                     "STSO 4400 - Medicine, Culture, and Society",
+--                     "STSO 4560 - Gender, Science, and Technology"}'
+-- WHERE id = 45;
+
+-- INSERT INTO CLASSES(category) VALUES
+-- ('"Transfer Student"'); 
                 
--- INSERT INTO CDI(choose, user_id) VALUES (
---     '"IHSS 1040 - Documentary in the 21st Century Identity Production"', 3
+-- INSERT INTO PATHWAYS(classname, firstchoose, nextchoose, minors, user_id) VALUES (
+--     '"Transfer Student Social Sciece"', 
+--                     -- first choose option
+--                   '{"COGS XXXX",
+--                     "ECON XXXX",
+--                     "GSAS XXXX",
+--                     "IHSS XXXX",
+--                     "PSYC XXXX",
+--                     "STSS XXXX"}', 
+--                     -- next choose option
+--                     '{}', 
+--                     -- minors
+--                     '{}', 
+--                     -- id key
+--                     10
 -- );
 
--- ALTER TABLE CDI
--- RENAME choose TO available;
+-- UPDATE PATHWAYS
+-- SET optionone = '{"Choose 12 credits of the following course prefixes, with at least 8 credit hours at, or above, the 2000 level and at least 3 credit hours at the 4000 level:"}'
+-- WHERE id = 48;
+
+-- UPDATE PATHWAYS
+-- SET optiontwo = '{"Choose remaining credits from the following:"}'
+-- WHERE id = 45;
+
+-- UPDATE PATHWAYS
+-- SET optiontwo = '{"Choose 12 credits from the following, with at least 4 credits at the 4000 level:"}'
+-- WHERE id = 37;
+
+-- UPDATE PATHWAYS
+-- SET optionone = '{"Choose 12 credits from the following:"}'
+-- WHERE id = 42;
+
+-- UPDATE PATHWAYS
+-- SET optiontwo = '{"Choose other one of the following:"}'
+-- WHERE id = 17;
+
+-- UPDATE PATHWAYS
+-- SET optionthree = '{}'
+-- WHERE optionthree IS NULL;
+
+-- UPDATE PATHWAYS
+-- SET thirdchoose = '{}'
+-- WHERE thirdchoose IS NULL;
 
 
--- SELECT * FROM CLASSES JOIN CDI ON CDI.user_id = CLASSES.id;
+
+-- SELECT * FROM CLASSES JOIN PATHWAYS ON PATHWAYS.user_id = CLASSES.id;
 
 -- SELECT EXISTS (
 --     SELECT FROM 
---         CLASSES
+--         PATHWAYS
 --     WHERE 
---          = ' '
+--         firstchoose = '"IHSS 1960 – Music and Protest"'
 --     );
 
--- DELETE FROM CLASSES
--- WHERE required = ' ';
+-- DELETE FROM PATHWAYS
+-- WHERE classname = '"Fact and Fiction"';
 
 -- SELECT *
 -- FROM CLASSES
 -- JOIN CDI2 ON CDI2.choose = CLASSES.choose;
 
--- SELECT * FROM CDI;
+-- 
+SELECT * FROM PATHWAYS;
 SELECT * FROM CLASSES;
+
+
