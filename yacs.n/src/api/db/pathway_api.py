62581af3090900e0a1526d9152105ba498ab1e43
chickenwang6 <122407085+chickenwang6@users.noreply.github.com>
2023-04-16 21:52:30 -0400

    figured out (?) how to print values, minor issues with null

--- yacs.n/src/api/db/pathway_api.py
+++ yacs.n/src/api/db/pathway_api.py
@@ -1,47 +1,82 @@
 import psycopg2
  
 # establishing the connection
-conn = psycopg2.connect(
-   database="test",
-    user='postgres',
-    password='password',
-    host='localhost',
-    port= '5432'
-)
- 
+
+cur = None
+conn = None
+try:
+    conn = psycopg2.connect(
+        database="postgres",
+        user='postgres',
+        password='Fliesrock7',
+        host='localhost',
+        port= '5433')
+    cur = conn.cursor()
+
+    postgreSQL_select_Query = "select * from pathwayname"
+    postgreSQL_select_Query2 = "select * from requirementsforpath"
+
+    cur.execute(postgreSQL_select_Query)
+    print("Selecting rows from paths table using cursor.fetchall")
+    pathwayTable = cur.fetchall()
+
+    print("Print each row and it's columns values")
+    for row in pathwayTable:
+        print("name of path = ", row[1])
+        print("compatible minors  = ", row[2], "\n")
+
+    cur.execute(postgreSQL_select_Query2)
+    print("Selecting rows from requirements table using cursor.fetchall")
+    allrequirements = cur.fetchall()
+    for row in allrequirements:
+        print("chooses = ", row[1], "\n")
+        print("classone = ", row[2], "\n")
+        if row[3] is not None:
+            print("classtwo = ", row[3], "\n")
+        if row[4] is not None:
+            print("classthree = ", row[4], "\n")
+
+except Exception as error:
+    print(error)
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
+
 # Creating a cursor object using the cursor()
 # method
-cursor = conn.cursor()
+# cursor = conn.cursor()
  
-sql = '''CREATE TABLE WORKER(
-   ID BIGSERIAL NOT NULL PRIMARY KEY,
-   NAME VARCHAR(100) NOT NULL,
-   COUNTRY VARCHAR(50) NOT NULL,
-   AGE INT,
-   SALARY FLOAT   
-)'''
-cursor.execute(sql)
+# sql = '''CREATE TABLE WORKER(
+#    ID BIGSERIAL NOT NULL PRIMARY KEY,
+#    NAME VARCHAR(100) NOT NULL,
+#    COUNTRY VARCHAR(50) NOT NULL,
+#    AGE INT,
+#    SALARY FLOAT   
+# )'''
+# cursor.execute(sql)
  
 # Inserting values into the table
-insert_stmt = "INSERT INTO WORKER (NAME, COUNTRY,\
-AGE, SALARY) VALUES (%s, %s, %s, %s)"
-data = [('Krishna', 'India', 19,2000),
-   ('Harry', 'USA', 20,7000),
-   ('Malang', 'Nepal', 25, 5000),
-   ('Apple', 'London', 26, 2000),
-   ('Vishnu', 'India', 29,2000),
-   ('Frank', 'UAE', 21,7000),
-   ('Master', 'USA', 25, 5000),
-   ('Montu', 'India', 26, 2000),
-        ]
-cursor.executemany(insert_stmt, data)
+# insert_stmt = "INSERT INTO WORKER (NAME, COUNTRY,\
+# AGE, SALARY) VALUES (%s, %s, %s, %s)"
+# data = [('Krishna', 'India', 19,2000),
+#    ('Harry', 'USA', 20,7000),
+#    ('Malang', 'Nepal', 25, 5000),
+#    ('Apple', 'London', 26, 2000),
+#    ('Vishnu', 'India', 29,2000),
+#    ('Frank', 'UAE', 21,7000),
+#    ('Master', 'USA', 25, 5000),
+#    ('Montu', 'India', 26, 2000),
+#         ]
+# cursor.executemany(insert_stmt, data)
  
 # Display whole table
-cursor.execute("SELECT * FROM WORKER")
-print(cursor.fetchall())
+# cursor.execute("SELECT * FROM WORKER")
+# print(cursor.fetchall())
  
 # Commit your changes in the database
-conn.commit()
+# conn.commit()
  
 #Closing the connection
 conn.close()
