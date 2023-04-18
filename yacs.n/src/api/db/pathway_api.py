a1cb63ba14a34c70839b2a170859159a79a09fd8
chickenwang6 <122407085+chickenwang6@users.noreply.github.com>
2023-04-17 22:11:23 -0400

    Able to access all the elements of the combined table!

--- yacs.n/src/api/db/pathway_api.py
+++ yacs.n/src/api/db/pathway_api.py
@@ -13,7 +13,7 @@ try:
         port= '5433')
     cur = conn.cursor()
 
-    postgreSQL_select_Query = "select * from pathwayname"
+    postgreSQL_select_Query = "SELECT COMBINE.majorname, PATHWAYNAME.nameofpath, (requirementsforpath.chooses[1]), REQUIREMENTSFORPATH.classone, (requirementsforpath.chooses[2]), REQUIREMENTSFORPATH.classtwo, (requirementsforpath.chooses[3]), REQUIREMENTSFORPATH.classthree, PATHWAYNAME.compatibleminors FROM COMBINE JOIN PATHWAYNAME ON PATHWAYNAME.id = COMBINE.require_id JOIN requirementsforpath ON REQUIREMENTSFORPATH.id = COMBINE.pathway_id"
     postgreSQL_select_Query2 = "select * from requirementsforpath"
 
     cur.execute(postgreSQL_select_Query)
@@ -22,19 +22,26 @@ try:
 
     print("Print each row and it's columns values")
     for row in pathwayTable:
-        print("name of path = ", row[1])
-        print("compatible minors  = ", row[2], "\n")
+        print("major name = ", row[0])
+        print("pathway  = ", row[1], "\n")
+        print("choose 1 = ", row[2])
+        print("1  = ", row[3], "\n")
+        print("choose 2 = ", row[4])
+        print("2  = ", row[5], "\n")
+        print("choose 3 = ", row[6])
+        print("3  = ", row[7], "\n")
+        print("minors = ", row[8])
 
-    cur.execute(postgreSQL_select_Query2)
-    print("Selecting rows from requirements table using cursor.fetchall")
-    allrequirements = cur.fetchall()
-    for row in allrequirements:
-        print("chooses = ", row[1], "\n")
-        print("classone = ", row[2], "\n")
-        if row[3] is not None:
-            print("classtwo = ", row[3], "\n")
-        if row[4] is not None:
-            print("classthree = ", row[4], "\n")
+    # cur.execute(postgreSQL_select_Query2)
+    # print("Selecting rows from requirements table using cursor.fetchall")
+    # allrequirements = cur.fetchall()
+    # for row in allrequirements:
+    #     print("chooses = ", row[1], "\n")
+    #     print("classone = ", row[2], "\n")
+    #     if row[3] is not None:
+    #         print("classtwo = ", row[3], "\n")
+    #     if row[4] is not None:
+    #         print("classthree = ", row[4], "\n")
 
 except Exception as error:
     print(error)
